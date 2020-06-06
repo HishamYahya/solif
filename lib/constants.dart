d283011e025da5db1b97500126dcb40b749c1e34
HishamYahya <38282695+HishamYahya@users.noreply.github.com>
2020-06-07 01:59:34 +0300

    initial design for add screen

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,20 @@
 import 'package:flutter/material.dart';
 
 const kMainColor = Colors.blueGrey;
+
+const kHeadingTextStyle = TextStyle(
+  fontSize: 40,
+  color: Colors.white,
+  fontWeight: FontWeight.bold,
+);
+
+const kTextFieldBorder = UnderlineInputBorder(
+  borderSide: BorderSide(
+    color: Colors.white,
+  ),
+);
+
+final kHintTextStyle = TextStyle(
+  fontSize: 30,
+  color: Colors.white54,
+);
