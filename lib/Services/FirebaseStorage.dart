5f70553122314b27c6b6a8622883de87a2f4b757
MohammedAlkhrashi <mohammad-x-@hotmail.com>
2020-09-25 11:43:19 +0300

    image upload functions

--- lib/Services/FirebaseStorage.dart
+++ lib/Services/FirebaseStorage.dart
@@ -1,9 +1,24 @@
-// import 'dart:html';
-// import 'package:firebase_storage/firebase_storage.dart';
+import 'dart:io';
 
-// class FirebaseStorage {
-//   /// stores a file in Firebase Storage and returns it's url.
-//   static String storeFile(File file, String salfhID) {
-//     final StorageReference storageReference = FirebaseStorage().ref().child();
-//   }
-// }
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/cupertino.dart';
+
+/// Take an image and a salfh ID.
+/// Saves the image in a folder that has the salfhID as a name.
+/// returns the image URL to store in cloud firestore.
+class StorageFunctions {
+  static Future<String> uploadImage({File imageFile, String salfhID}) async {
+    final StorageReference firebaseStorageRef = FirebaseStorage.instance
+        .ref()
+        .child('images/${salfhID}/${DateTime.now().toString()}');
+    StorageUploadTask uploadTask = firebaseStorageRef.putFile(imageFile);
+    StorageTaskSnapshot storageSnapshot = await uploadTask.onComplete;
+
+    String downloadUrl = await storageSnapshot.ref.getDownloadURL();
+
+    if (uploadTask.isComplete) {
+      return downloadUrl.toString();
+    }
+    return null;
+  }
+}
